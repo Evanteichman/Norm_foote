bd9e20ea27ac753d57e64e29b3fc685faa05d7d5
Hunter <81187405+Hunter-Paulson@users.noreply.github.com>
2021-07-29 15:01:09 -0700

    Adding Single Foote Notes

---
+++ single-nf-foote-note.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package norm_foote_theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post(); ?>
+
+			<article>
+				<?php if ( function_exists ( 'get_field' ) ) : ?>
+
+					<h1><?php the_title(); ?></h1> 
+							
+					<?php if ( has_post_thumbnail() ) :
+						the_post_thumbnail();	
+					endif; ?>
+							
+					<?php if ( get_field( 'text' ) ) : ?>
+						<p><?php the_field('text'); ?></p>	
+					<?php endif; ?>
+							
+				<?php endif; ?>
+								
+			</article> <?php
+
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'nf_theme' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'nf_theme' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+// get_sidebar();
+get_footer();
