c3d42acb5d67409bc4babdc5148980c051d075b3
Evanteichman <81187144+Evanteichman@users.noreply.github.com>
2021-08-18 08:56:52 -0700

    simple 404 page

--- 404.php
+++ 404.php
@@ -18,38 +18,14 @@ get_header();
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'nf_theme' ); ?></p>
+				<p class="page-not-found">Unfornuately this page cannot be found! Please find the page you're looking for in the menu either located at the top or bottom of your screen.</p>
+				
 
-					<?php
-					get_search_form();
+					
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+					
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'nf_theme' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$nf_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'nf_theme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$nf_theme_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
